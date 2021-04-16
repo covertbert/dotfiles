36459870498c1da4bea68933b1d53e6a99727362
Bertie Blackman <blackmanrgh@gmail.com>
2021-04-16 10:05:25 +0100

    Adds bpytop

--- config/zsh/aliases.zsh
+++ config/zsh/aliases.zsh
@@ -18,6 +18,8 @@ alias grep='ggrep'
 
 alias t='tail -f'
 
+alias top='bpytop'
+
 # Make zsh know about hosts already accessed by SSH
 zstyle -e ':completion:*:(ssh|scp|sftp|rsh|rsync):hosts' hosts 'reply=(${=${${(f)"$(cat {/etc/ssh_,~/.ssh/known_}hosts(|2)(N) /dev/null)"}%%[# ]*}//,/ })'
 
