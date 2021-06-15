125dcc8c9f466c8020773da191f2ac7e71fde4fa
Bertie Blackman <blackmanrgh@gmail.com>
2021-06-15 09:16:55 +0100

    Changes font to Fira Code

--- config/.hyper.js
+++ config/.hyper.js
@@ -15,8 +15,7 @@ module.exports = {
     fontSize: 15,
 
     // font family with optional fallbacks
-    fontFamily:
-      'JetBrainsMono Nerd Font, Consolas, "Lucida Console", monospace',
+    fontFamily: 'Fira Code, Consolas, "Lucida Console", monospace',
 
     // default font weight: 'normal' or 'bold'
     fontWeight: "normal",
