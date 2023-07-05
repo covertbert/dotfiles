291980babe0e2ec1d4723b2b15fcac60e710db72
Bertie Blackman <bertie.blackman@airtimerewards.com>
2023-07-05 17:00:18 +0100

    Updates brew stuff

--- packages.sh
+++ packages.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-brew bundle --file=./brew/Brewfile
-brew bundle --file=./brew/Caskfile
+brew bundle --verbose --file=./brew/Brewfile
+brew bundle --verbose --file=./brew/Caskfile
+
+"$(brew --prefix)"/opt/fzf/install
